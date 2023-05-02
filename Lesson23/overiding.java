3dfb96ef4833e84d5a038efc404a9542de3309bc
VanPirg <ivan.files.09@gmail.com>
2023-05-02 23:36:10 +0300

    Lesson23 Overiding

---
+++ Lesson23/overiding.java
@@ -0,0 +1,22 @@
+package Lesson23;
+public class overiding {
+    public static void main(String[] args) {
+        Emloyee e = new Emloyee();
+        Emloyee e1= new teacher();
+        e1.eat();
+        e.eat();
+    }
+}
+class Emloyee{
+    String name;
+    int age;
+    int expirience;
+    void eat(){System.out.println("Kushaet rabotnik");}
+    void sleep(){System.out.println("Spat");}
+}
+class teacher extends Emloyee {
+    String kolichestvoUchenikov;
+    void uchit(){System.out.println("Uchit");}
+    void eat(){System.out.println("Kushaet uchitel");}
+
+}
